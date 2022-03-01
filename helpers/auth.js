dd1589ec00536aec3b66c8ff22a146dbaf207efa
ahmedsamir4083 <58106888+ahmedsamir4083@users.noreply.github.com>
2022-03-01 12:31:14 +0200

    Delete helpers directory

--- helpers/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports.isloggedin = (req, res , next ) =>{
-    if(!req.isAuthenticated()){
-        req.flash('error','You must be signed in first !');
-        return res.redirect('/loginpage');
-    }
-    next();
-}
